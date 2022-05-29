cf2b1a69088c7095c5fba2c18532bbbe9de146bb
JongHoP <74240296+JongHoP@users.noreply.github.com>
2022-05-29 21:45:34 +0900

    add_ui
    가게 선택 화면 추가

---
+++ app/src/main/java/com/example/customlistview/RecyclerViewItem.java
@@ -0,0 +1,15 @@
+package com.example.customlistview;
+
+
+public class RecyclerViewItem {
+
+    private String mMainText;
+
+    public String getSubText() {
+        return mMainText;
+    }
+
+    public void setSubText(String subText) {
+        this.mMainText = subText;
+    }
+}
