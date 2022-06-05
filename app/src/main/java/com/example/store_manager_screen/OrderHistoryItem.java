dd262874303d75597bb4d35a27bd79912a76d762
csj <koreaxman@naver.com>
2022-06-05 14:56:14 +0900

    주문내역 화면

---
+++ app/src/main/java/com/example/store_manager_screen/OrderHistoryItem.java
@@ -0,0 +1,31 @@
+package com.example.store_manager_screen;
+
+public class OrderHistoryItem {
+    private String menuName;
+    private int menuNum;
+    private int menuPrice;
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public int getMenuNum() {
+        return menuNum;
+    }
+
+    public void setMenuNum(int menuNum) {
+        this.menuNum = menuNum;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public void setMenuPrice(int menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+}
